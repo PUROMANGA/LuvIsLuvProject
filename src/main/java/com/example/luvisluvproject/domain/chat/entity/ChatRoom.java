1cc2f001c193a39b2587bc5ac453db4f9515bd70
JIN <wlsqkrtk11@naver.com>
2025-05-28 19:05:17 +0900

    # feat : ChatRoom entity 작성

--- src/main/java/com/example/luvisluvproject/domain/chat/entity/ChatRoom.java
+++ src/main/java/com/example/luvisluvproject/domain/chat/entity/ChatRoom.java
@@ -7,6 +7,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -15,6 +16,7 @@ import lombok.NoArgsConstructor;
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
+@Table(name = "ChatRooms")
 
 public class ChatRoom extends BaseEntity {
 
