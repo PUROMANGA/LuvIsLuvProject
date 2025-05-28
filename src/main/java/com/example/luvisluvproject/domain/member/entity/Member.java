f28d951d5732b89493a2385f75c285f74e5522f7
Jaehoon <bjh0164@naver.com>
2025-05-28 19:30:51 +0900

    refactor: Member Entity 제약조건 추가

--- src/main/java/com/example/luvisluvproject/domain/member/entity/Member.java
+++ src/main/java/com/example/luvisluvproject/domain/member/entity/Member.java
@@ -1,7 +1,9 @@
 package com.example.luvisluvproject.domain.member.entity;
 
+import com.example.luvisluvproject.domain.member.enums.UserRole;
 import com.example.luvisluvproject.global.common.BaseEntity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -20,16 +22,22 @@ public class Member extends BaseEntity {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
+	@Column(nullable = false, length = 50)
 	private String name;
 
+	@Column(unique = true, nullable = false)
 	private String email;
 
+	@Column(nullable = false)
 	private String password;
 
+	@Column(nullable = false, length = 10)
 	private String birthday;
 
-	private String userRole;
+	@Column(nullable = false)
+	private UserRole userRole;
 
+	@Column(nullable = false)
 	private boolean status;
 
 }
