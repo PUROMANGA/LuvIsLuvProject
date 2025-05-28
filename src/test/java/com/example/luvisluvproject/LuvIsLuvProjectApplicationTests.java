205ae642b652ee6ef491b98cbf6246326351025c
Ciel415 <yun100778@gmail.com>
2025-05-28 16:57:32 +0900

    CI 테스트1

--- src/test/java/com/example/luvisluvproject/LuvIsLuvProjectApplicationTests.java
+++ src/test/java/com/example/luvisluvproject/LuvIsLuvProjectApplicationTests.java
@@ -2,8 +2,10 @@ package com.example.luvisluvproject;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class LuvIsLuvProjectApplicationTests {
 
 	@Test
