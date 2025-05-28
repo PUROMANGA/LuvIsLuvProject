087d0c8c11f98a546377d9d40a336e415fa453be
JIN <wlsqkrtk11@naver.com>
2025-05-28 19:08:13 +0900

    # feat : ChatRoom 서비스 로직 작성

---
+++ src/main/java/com/example/luvisluvproject/domain/chat/controller/ChatRoomController.java
@@ -0,0 +1,11 @@
+package com.example.luvisluvproject.domain.chat.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequiredArgsConstructor
+
+public class ChatRoomController {
+}
