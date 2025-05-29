c665615a55cb1b0b2b99e85be1a73d0779deab00
JIN <wlsqkrtk11@naver.com>
2025-05-29 18:43:14 +0900

    # fix : dev 후 MatchService 리팩토링

--- src/main/java/com/example/luvisluvproject/domain/match/Service/MatchService.java
+++ src/main/java/com/example/luvisluvproject/domain/match/Service/MatchService.java
@@ -1,5 +1,6 @@
 package com.example.luvisluvproject.domain.match.Service;
 
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
 import com.example.luvisluvproject.domain.match.dto.AcceptMatchDto;
@@ -21,6 +22,7 @@ public class MatchService {
 
 	private final MatchRepository matchRepository;
 	private final MemberRepository memberRepository;
+	private final RedisTemplate<String, Object> redisTemplate;
 
 	/**
 	 * 매칭을 걸면 해당 사람에게 요청이 갑니다.
@@ -31,16 +33,19 @@ public class MatchService {
 
 	public MatchResponseDto createMatchService(MatchRequestDto matchRequestDto, String email) {
 		//senderId(로그인된 유저)
-		Member senderMember = memberRepository.findByEmail(email);
+		Member senderMember = memberRepository.findByEmail(email).orElseThrow(() -> new CustomRuntimeException(
+			ExceptionCode.USER_CANT_FIND));
 		//receiverId(좋아요 받은 유저)
 		Member receiverMember = memberRepository.findById(matchRequestDto.getReceiverId()).orElseThrow(() -> new CustomRuntimeException(
 			ExceptionCode.USER_CANT_FIND));
 		//match(수락 안 된 상태) 객체 생성
 		Match match = new Match(senderMember.getId(), receiverMember.getId());
 		//match를 저장합니다.
-		matchRepository.save(match);
+		String redisKey = senderMember.getId().toString() + " : " + receiverMember.getId().toString();
+		redisTemplate.opsForSet().add(redisKey, match);
 		return new MatchResponseDto(match);
 	}
+
 
 	/**
 	 * 해당 매칭이 오고, 매칭의 상태를 '받음'으로 변경합니다.
@@ -49,6 +54,7 @@ public class MatchService {
 	 * @return
 	 */
 	public MatchResponseDto patchMatchService(Long matchId, String email) {
+		redisTemplate.opsForSet().members()
 		//매칭 찾고
 		Match match = matchRepository.findById(matchId).orElseThrow(() -> new CustomRuntimeException(ExceptionCode.CANT_FIND_INTERFACE));
 		//매칭 상태를 변경
