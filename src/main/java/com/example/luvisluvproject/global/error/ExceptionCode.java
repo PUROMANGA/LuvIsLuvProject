a1e20a49600574c3ff82ec80d1212118e02de0ae
JIN <wlsqkrtk11@naver.com>
2025-05-29 17:19:55 +0900

    # feat : ExceptionCode 리팩토링

--- src/main/java/com/example/luvisluvproject/global/error/ExceptionCode.java
+++ src/main/java/com/example/luvisluvproject/global/error/ExceptionCode.java
@@ -11,6 +11,10 @@ import lombok.RequiredArgsConstructor;
 public enum ExceptionCode implements ErrorCode {
 
 	VALID_EXCEPTION(HttpStatus.BAD_REQUEST, "VALID_EXCEPTION가 발생했습니다, 인텔리제이 로그를 확인해주세요"),
+	//가게
+	STORE_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 가게를 찾을 수 없습니다."),
+	CANT_FIND_INTERFACE(HttpStatus.NOT_FOUND, "해당 정보를 찾을 수 없습니다"),
+	//유저(=멤버)
 	USER_CANT_FIND(HttpStatus.NOT_FOUND, "해당 유저를 찾을 수 없습니다");
 
 	private final HttpStatus httpStatus;
