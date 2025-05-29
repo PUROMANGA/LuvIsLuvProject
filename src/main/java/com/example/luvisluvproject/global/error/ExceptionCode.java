004ef8143c9f8a3856821ed20fb093320a82da0e
Jaehoon <bjh0164@naver.com>
2025-05-29 18:58:57 +0900

    refactor: user error code 추가

--- src/main/java/com/example/luvisluvproject/global/error/ExceptionCode.java
+++ src/main/java/com/example/luvisluvproject/global/error/ExceptionCode.java
@@ -14,14 +14,18 @@ public enum ExceptionCode implements ErrorCode {
 
 	// auth
 	EMAIL_ALREADY_EXIST(HttpStatus.CONFLICT, "중복된 이메일입니다."),
-	USER_ROLE_NOT_FOUND(HttpStatus.NOT_FOUND, "지원하지 않는 사용자 역할입니다.");
+	USER_ROLE_NOT_FOUND(HttpStatus.NOT_FOUND, "지원하지 않는 사용자 역할입니다."),
 
 	//가게
 	STORE_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 가게를 찾을 수 없습니다."),
 	CANT_FIND_INTERFACE(HttpStatus.NOT_FOUND, "해당 정보를 찾을 수 없습니다"),
-  
+
 	//유저(=멤버)
-	USER_CANT_FIND(HttpStatus.NOT_FOUND, "해당 유저를 찾을 수 없습니다");
+	USER_CANT_FIND(HttpStatus.NOT_FOUND, "해당 유저를 찾을 수 없습니다"),
+	MEMBER_NOT_FOUND(HttpStatus.NOT_FOUND, "존재하지 않는 사용자입니다."),
+	LOGIN_REQUIRED(HttpStatus.UNAUTHORIZED, "로그인이 필요합니다."),
+	SAME_PASSWORD(HttpStatus.BAD_REQUEST, "이전과 동일한 비밀번호는 사용이 불가합니다."),
+	PASSWORD_MISMATCH(HttpStatus.BAD_REQUEST, "입력한 비밀번호가 일치하지 않습니다.");
 
 	private final HttpStatus httpStatus;
 	private final String message;
