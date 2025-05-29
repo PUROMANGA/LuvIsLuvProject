6a0bd47004e2f6272dcf76b5dfb82e42f872bc0a
JIN <wlsqkrtk11@naver.com>
2025-05-29 15:30:56 +0900

    # feat : ExceptionCode 추가(유저 정보 찾을 수 없음)

--- src/main/java/com/example/luvisluvproject/global/error/ExceptionCode.java
+++ src/main/java/com/example/luvisluvproject/global/error/ExceptionCode.java
@@ -10,7 +10,8 @@ import lombok.RequiredArgsConstructor;
 
 public enum ExceptionCode implements ErrorCode {
 
-	VALID_EXCEPTION(HttpStatus.BAD_REQUEST, "VALID_EXCEPTION가 발생했습니다, 인텔리제이 로그를 확인해주세요");
+	VALID_EXCEPTION(HttpStatus.BAD_REQUEST, "VALID_EXCEPTION가 발생했습니다, 인텔리제이 로그를 확인해주세요"),
+	USER_CANT_FIND(HttpStatus.NOT_FOUND, "해당 유저를 찾을 수 없습니다");
 
 	private final HttpStatus httpStatus;
 	private final String message;
