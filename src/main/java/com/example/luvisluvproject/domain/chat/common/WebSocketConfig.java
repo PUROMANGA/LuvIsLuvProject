ba6e39c7b032f07bfc209612d32b82a456ecff60
JIN <wlsqkrtk11@naver.com>
2025-05-29 12:25:55 +0900

    # feat : WebSocketConfig작성, interceptorHandler작성

---
+++ src/main/java/com/example/luvisluvproject/domain/chat/common/WebSocketConfig.java
@@ -0,0 +1,29 @@
+package com.example.luvisluvproject.domain.chat.common;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+import lombok.RequiredArgsConstructor;
+
+@Configuration
+@RequiredArgsConstructor
+@EnableWebSocketMessageBroker
+
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+	@Override
+	public void registerStompEndpoints(StompEndpointRegistry registry) {
+		registry.addEndpoint("/ws")
+			.setAllowedOriginPatterns("*")
+			.withSockJS();
+	}
+
+	@Override
+	public void configureMessageBroker(MessageBrokerRegistry registry) {
+		registry.enableSimpleBroker("/sub");
+		registry.setApplicationDestinationPrefixes("/pub");
+	}
+}
