d0e536db84fa6332d4f4a923929acc0f4adf3db7
Ciel415 <yun100778@gmail.com>
2025-05-30 18:17:45 +0900

    ✅ BlockServiceTest 정상 작동 확인 및 테스트 코드 푸시

--- src/main/java/com/example/luvisluvproject/domain/block/controller/BlockController.java
+++ src/main/java/com/example/luvisluvproject/domain/block/controller/BlockController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/blocks")
+@RequestMapping("/blocks") // 수정완료
 @RequiredArgsConstructor
 public class BlockController {
 
