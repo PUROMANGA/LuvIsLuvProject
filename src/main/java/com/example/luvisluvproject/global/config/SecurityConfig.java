ab6f105a56f6c8195b105f438ea8fe30de741d4f
bella <yiseuljeong0415@gmail.com>
2025-06-02 19:45:23 +0900

    Changes by sonhochan

--- src/main/java/com/example/luvisluvproject/global/config/SecurityConfig.java
+++ src/main/java/com/example/luvisluvproject/global/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.example.luvisluvproject.global.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -10,6 +11,7 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
+@EnableJpaAuditing
 public class SecurityConfig {
 
 	// 인증 미적용할 부분 필터링
