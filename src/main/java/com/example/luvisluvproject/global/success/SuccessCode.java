0f7770613b70d522797e4856687a2577b4f8ca95
Jaehoon <bjh0164@naver.com>
2025-06-02 22:47:02 +0900

    feat: 리뷰 성공 코드

--- src/main/java/com/example/luvisluvproject/global/success/SuccessCode.java
+++ src/main/java/com/example/luvisluvproject/global/success/SuccessCode.java
@@ -8,9 +8,17 @@ import lombok.RequiredArgsConstructor;
 @Getter
 @RequiredArgsConstructor
 public enum SuccessCode {
+	//회원
 	FIND_MEMBER_SUCCESS(HttpStatus.OK, "회원 조회 성공"),
 	UPDATE_MEMBER_SUCCESS(HttpStatus.OK, "회원정보 수정 성공"),
-	DELETE_MEMBER_SUCCESS(HttpStatus.OK, "회원 탈퇴 완료");
+	DELETE_MEMBER_SUCCESS(HttpStatus.OK, "회원 탈퇴 완료"),
+
+	//리뷰
+	CREATE_REVIEW_SUCCESS(HttpStatus.OK, "리뷰 작성 성공"),
+	UPDATE_REVIEW_SUCCESS(HttpStatus.OK, "리뷰 수정 성공"),
+	GET_REVIEW_SUCCESS(HttpStatus.OK, "리뷰 상세 조회 성공"),
+	GET_ALL_REVIEWS_SUCCESS(HttpStatus.OK, "리뷰 전체 조회 성공"),
+	DELETE_REVIEW_SUCCESS(HttpStatus.OK, "리뷰 전체 조회 성공");
 
 	private final HttpStatus httpStatus;
 	private final String message;
