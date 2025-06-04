2d8c0aea9258c246b9d896c4694ef80987970ae5
JIN <wlsqkrtk11@naver.com>
2025-06-04 12:24:30 +0900

    # 리팩토링 : 필요없는 거 제거함

--- src/main/java/com/example/luvisluvproject/global/config/SecurityConfig.java
+++ src/main/java/com/example/luvisluvproject/global/config/SecurityConfig.java
@@ -2,7 +2,6 @@ package com.example.luvisluvproject.global.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
