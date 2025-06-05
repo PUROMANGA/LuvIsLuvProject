8c92498b5d0d6264600154b379561fc3758d6cfa
J <justonesip82@gmail.com>
2025-06-05 15:58:30 +0900

    refactor: 회원가입, 로그인, 로그아웃 성공시 ApiResponse 추가

--- src/main/java/com/example/luvisluvproject/global/success/SuccessCode.java
+++ src/main/java/com/example/luvisluvproject/global/success/SuccessCode.java
@@ -8,6 +8,10 @@ import lombok.RequiredArgsConstructor;
 @Getter
 @RequiredArgsConstructor
 public enum SuccessCode {
+	SIGNUP_SUCCESS(HttpStatus.OK, "회원가입 완료"),
+	LOGIN_SUCCESS(HttpStatus.OK, "로그인 성공"),
+	LOGOUT_SUCCESS(HttpStatus.OK, "로그아웃 성공"),
+
 	FIND_MEMBER_SUCCESS(HttpStatus.OK, "회원 조회 성공"),
 	UPDATE_MEMBER_SUCCESS(HttpStatus.OK, "회원정보 수정 성공"),
 	DELETE_MEMBER_SUCCESS(HttpStatus.OK, "회원 탈퇴 완료");
