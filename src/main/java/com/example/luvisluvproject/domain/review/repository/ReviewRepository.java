39f8fbaa2b436326907b5cdabd8f5d36ea4317f0
Jaehoon <bjh0164@naver.com>
2025-06-11 15:35:59 +0900

    feat: 리뷰 전체 조회에 Pageable 적용

--- src/main/java/com/example/luvisluvproject/domain/review/repository/ReviewRepository.java
+++ src/main/java/com/example/luvisluvproject/domain/review/repository/ReviewRepository.java
@@ -1,13 +1,12 @@
 package com.example.luvisluvproject.domain.review.repository;
 
-import java.util.List;
-
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.luvisluvproject.domain.review.entity.Review;
 import com.example.luvisluvproject.domain.store.entity.Store;
 
 public interface ReviewRepository extends JpaRepository<Review, Long> {
-	List<Review> findByStoreId(Store store);
-
+	Page<Review> findByStoreId(Store store, Pageable pageable);
 }
